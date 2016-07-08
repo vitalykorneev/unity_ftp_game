ef532f5e128d994b218929bbe592806cbff99995
Vitaly Korneev <korneev@factory.mn>
2016-07-08 14:09:18 +0300

    add ray shooter mod to player camera

---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera> ();
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+			Ray ray = _camera.ScreenPointToRay (point);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log ("Hit " + hit.point);
+				StartCoroutine (SphereIndicator (hit.point));
+			}
+		}
+	}
+
+	void OnGUI() {
+		int size = 1;
+		float posX = _camera.pixelWidth / 2 - size / 4;
+		float posY = _camera.pixelHeight / 2 - size / 2;
+
+		GUI.Label (new Rect (posX, posY, size, size), "+");
+	}
+
+	private IEnumerator SphereIndicator(Vector3 pos) {
+		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+
+		yield return new WaitForSeconds (1);
+
+		Destroy (sphere);
+	}
+}
