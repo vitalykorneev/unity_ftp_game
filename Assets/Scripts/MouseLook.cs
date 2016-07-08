1fd94f42813f967d8f18931747de9760acef5a8f
Vitaly Korneev <redeffect.by@gmail.com>
2016-07-08 08:09:47 +0300

    create unity project

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityHor = 9.0f;
+	public float sensitivityVert = 9.0f;
+	public float minVert = -45.0f;
+	public float maxVert = 45.0f;
+
+	private float _rotationX = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			transform.Rotate (0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			_rotationX -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp (_rotationX, minVert, maxVert);
+
+			float rotationY = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+		} else {
+			_rotationX -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp (_rotationX, minVert, maxVert);
+
+			float delta = Input.GetAxis ("Mouse X") * sensitivityHor;
+			float rotationY = transform.localEulerAngles.y + delta;
+
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+		}
+	}
+}
