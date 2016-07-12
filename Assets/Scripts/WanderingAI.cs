3fadd33769b0a8f2ec61f75cc79dd360e3e0f257
Vitaly Korneev <redeffect.by@gmail.com>
2016-07-12 22:38:28 +0300

    create alive status of enemy

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -5,15 +5,31 @@ public class WanderingAI : MonoBehaviour {
 
 	public float speed = 3.0f;
 	public float obstacleRange = 5.0f;
+
+	private bool _alive;
+
+	void Start() {
+		_alive = true;
+	}
+
 	void Update() {
-		transform.Translate(0, 0, speed * Time.deltaTime);
-		Ray ray = new Ray(transform.position, transform.forward);
-		RaycastHit hit;
-		if (Physics.SphereCast(ray, 0.75f, out hit)) {
-			if (hit.distance < obstacleRange) {
-				float angle = Random.Range(-110, 110);
-				transform.Rotate(0, angle, 0);
+		if (_alive) {
+		
+			transform.Translate(0, 0, speed * Time.deltaTime);
+			Ray ray = new Ray(transform.position, transform.forward);
+			RaycastHit hit;
+			if (Physics.SphereCast(ray, 0.75f, out hit)) {
+				if (hit.distance < obstacleRange) {
+					float angle = Random.Range(-110, 110);
+					transform.Rotate(0, angle, 0);
+				}
 			}
+		
 		}
+
+	}
+
+	public void SetAlive(bool alive) {
+		_alive = alive;
 	}
 }
